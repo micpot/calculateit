94cabec925fd721c72f402eb830372197cb36eff
micpot <32093024+micpot@users.noreply.github.com>
2018-11-30 15:33:01 +0100

    first tries

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+console.log('hello');
+
 function Calculator (){
-    
+
 }
+
+Calculator.prototype.init = function() {
+    var btnObj = document.querySelectorAll('button');
+for (var i=0; i<btnObj.length; i++){
+    btnObj[i].addEventListener('click',function(){
+        //console.log(this.innerHTML);
+    if(this.classList.contains('num')) {
+        document.getElementsByClassName('item-header')[0].innerHTML = this.innerHTML;
+    } else if(this.classList.contains ('orange')){
+        calculationLogic.operations(this);
+    }
+})
+}
+}
+
+
+
+
+
+/*Calculator.prototype.fetchCurrentValue = function (){
+    return document.getElementsByClassName('item-header')[0].innerHTML;
+
+}
+
+Calculator.prototype.setCurrentValue = function (){
+    document.getElementsByClassName('item-sub-header')[0].innerHTML = value;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var calculationLogic = new Calculator();
+calculationLogic.init();
